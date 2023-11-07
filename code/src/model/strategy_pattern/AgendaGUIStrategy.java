7cfc59fe89eb2dcda6afea967ba278c9a45af392
cortesmc <carlosandres149@gmail.com>
2023-11-07 14:54:21 +0100

    SratPat_ListCalendar

--- code/src/model/strategy_pattern/AgendaGUIStrategy.java
+++ code/src/model/strategy_pattern/AgendaGUIStrategy.java
@@ -1,4 +1,10 @@
 package model.strategy_pattern;
 
+import javax.swing.*;
+import java.util.Calendar;
+
 public interface AgendaGUIStrategy {
+    public JPanel buildCalendar(Calendar calendar); // interface method (does not have a body)
+
+    JPanel buildCalendar(net.fortuna.ical4j.model.Calendar calendar);
 }
